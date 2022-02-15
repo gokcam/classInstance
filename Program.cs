14cca9fae7d843cfbd9115922db167ffb2fb2644
Gökhan Çam <76878009+gokcam@users.noreply.github.com>
2022-02-15 21:36:45 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Calisan calisan1 = new Calisan();
+            calisan1.Ad = "Gökhan";
+            calisan1.soyAd = "Çam";
+            calisan1.No=55;
+            calisan1.Departman="Yazılım";
+
+            calisan1.CalisanBilgileri();
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad;
+        public string soyAd;
+        public int No;
+        public string Departman;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Calisan Adi: {0}",Ad);
+            Console.WriteLine("Calisan Adi: {0}",soyAd);
+            Console.WriteLine("Calisan Adi: {0}",No);
+            Console.WriteLine("Calisan Adi: {0}",Departman);
+        }
+
+    }
+}
